d1354069afd19082b141f2491888b43cf3d7d9f6
obinnadtq <oisiwekpeni@gmail.com>
2022-01-31 23:16:48 +0100

    Refactor code to make it more readable and also validation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,25 +8,66 @@ public class Main {
     public static void main(String[] args) {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENTAGE = 100;
-
         System.out.println("Welcome to Obinna's Mortgage Calculator app ");
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        double principal = scanner.nextDouble();
+        double principal = validatePrincipal(scanner);
+        double interestRate = validateInterestRate(scanner, PERCENTAGE, MONTHS_IN_YEAR);
+        int durationInMonths = validateDuration(scanner, MONTHS_IN_YEAR);
 
-        System.out.print("Annual Interest rate in percentage: ");
-        double annualInterestRate = scanner.nextDouble();
-        double interestRate = annualInterestRate / PERCENTAGE / MONTHS_IN_YEAR;
+        double mortgagePayment = calculateMonthlyMortgage(principal, interestRate, durationInMonths);
+        printResult(mortgagePayment);
 
-        System.out.print("Duration in months: ");
-        int duration = scanner.nextInt();
+    }
 
+    public static double calculateMonthlyMortgage(double principal, double interestRate, int durationInMonths){
         double mortgagePaymentPerMonth = principal
-                * ((interestRate * Math.pow((1 + interestRate), duration))
-                / (Math.pow((1 + interestRate), duration) - 1));
+                * ((interestRate * Math.pow((1 + interestRate), durationInMonths))
+                / (Math.pow((1 + interestRate), durationInMonths) - 1));
+        return mortgagePaymentPerMonth;
+    }
 
-        String result = NumberFormat.getCurrencyInstance().format(mortgagePaymentPerMonth);
+    private static void printResult(double mortgage){
+        String result = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Your monthly mortgage payment is " + result);
+    }
+
+    private static double validatePrincipal(Scanner scanner){
+        double principal = 0;
+        while(true){
+            System.out.print("Principal (1k - 1M): ");
+            principal = scanner.nextDouble();
+            if(principal >= 1000 && principal <= 1000000 )
+                break;
+            System.out.println("Please enter a value between 1000 and 1000000");
+        }
+        return principal;
+    }
+
+    private static double validateInterestRate(Scanner scanner, int percentage, int monthsInYear){
+        double annualInterestRate = 0;
+        while(true){
+            System.out.print("Annual Interest rate in percentage: ");
+            annualInterestRate = scanner.nextDouble();
+            if(annualInterestRate > 0 && annualInterestRate <= 30 )
+                break;
+            System.out.println("Please enter a value greater than 0 and less than or equal to 30");
+        }
+        double interestRate = annualInterestRate / percentage / monthsInYear;
+        return interestRate;
+    }
+
+    private static int validateDuration(Scanner scanner, int monthsInYear){
+        int durationInMonths = 0;
+        while (true){
+            System.out.print("Duration in years: ");
+            int duration = scanner.nextInt();
+            if(duration > 0 && duration <= 15){
+                durationInMonths = duration * monthsInYear;
+                break;
+            }
+            System.out.println("Please enter a value between 1 and 15");
+        }
+        return durationInMonths;
     }
 }
